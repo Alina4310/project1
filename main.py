b7f35b310c6276ddcb315a2e608a0258e43f1dcd
Alina4310 <alina.ivanova11.11@mail.ru>
2025-06-03 14:26:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(10):
-    print("Hi!")
-
-for i in range(5):
-    print(i)
+for i in range(20,101):
+    if i % 10 == 1:
+        print(i, "манул")
+    if i % 10 ==2 or i % 10 ==3 or i % 10 == 4:
+        print(i, "манула")
+    if i % 10 == 5 or i 
