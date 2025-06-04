040ee516c9324cb9cb8b593e4b31ad591c74e3e1
Alina4310 <alina.ivanova11.11@mail.ru>
2025-06-04 11:33:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 question = "Какой сегодня месяц?"
 right_answer = "июнь"
-
+k = 0
 print(question)
 user_answer = input()
 while user_answer != right_answer:
     print("Wrong! Try again.")
     user_answer = input()
+    k = k + 1
+print("Well done, you used ",k," tries")
 
-print("Well done")
