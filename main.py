080fb65db4796250dccc77cfc294136f2236c72e
Alina4310 <alina.ivanova11.11@mail.ru>
2025-06-04 11:27:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-for i in range(20,101):
-    if i % 10 == 1:
-        print(i, "манул")
-    if i % 10 ==2 or i % 10 ==3 or i % 10 == 4:
-        print(i, "манула")
-    if i % 10 == 5 or i 
+question = "Какой сегодня месяц?"
+right_answer = "июнь"
+
+print(question)
+user_answer = input()
+while user_answer != right_answer:
+    print("Wrong! Try again.")
+    user_answer = input()
+
+print("Well done")
